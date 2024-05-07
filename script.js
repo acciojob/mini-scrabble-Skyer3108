77062f13f86233d412e09a2a267ae65fc435992b
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-05-07 16:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+
+const inpu=document.getElementById('evaluatedText')
+const coun=document.getElementById('letterCount')
+
+inpu.addEventListener('input',()=>{
+	console.log(inpu.value)
+	const letter=inpu.value
+	const count=letter.length;
+
+	coun.textContent=`${count}`
+	
+})
